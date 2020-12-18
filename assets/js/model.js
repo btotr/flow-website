abea59b88c1592a7c94666612d1171492bcfe3a6
Colin Meerveld <colin.meerveld@gmail.com>
2020-12-18 20:57:05 +0000

    added rdfs prefix

--- assets/js/model.js
+++ assets/js/model.js
@@ -16,7 +16,7 @@ Model.prototype.querySparql = function(sp, callback){
 			d.data.on('data', (a) => { res += a });
 			d.data.on('end', () => { 
 				console.log(res) ;
-				var prefix = "@prefix core:  <https://flow.recipes/ns/core#> .  @prefix skos: <http://www.w3.org/2008/05/skos#> . ";
+				var prefix = "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> . @prefix core:  <https://flow.recipes/ns/core#> .  @prefix skos: <http://www.w3.org/2008/05/skos#> . ";
 				self.trig2RDFXML(prefix + res, callback);
 			});
 		});
